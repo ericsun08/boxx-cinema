919b25c0b7395dadf28860370b37058a12d81734
ERIC SUN <eric.sun@mhealthbank.com>
2023-09-12 17:46:56 +0700

    change db config

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "database-1.cdk0qz7xfz4x.ap-southeast-2.rds.amazonaws.com",
     PORT: "1433",
     USER: "admin",
-    PASSWORD: "Ashwioon12!",
+    PASSWORD: "Password123!",
     DB: "Boxx_db",
     dialect: "mssql",
     pool: {
