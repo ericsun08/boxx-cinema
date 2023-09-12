70c8c232948af5abed59495556ebc7eef667d669
ERIC SUN <eric.sun@mhealthbank.com>
2023-09-12 18:26:04 +0700

    change port configuration

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ require('./routes/movieCast.routes')(app)
 require('./routes/searchShow.routes')(app)
 require('./routes/savedMovie.routes')(app)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
