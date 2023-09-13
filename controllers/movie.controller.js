a0c3270f0ef2dac2903752ac07fedad640ae97c4
ERIC SUN <eric.sun@mhealthbank.com>
2023-09-13 18:21:58 +0700

    undo fix path

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -29,9 +29,7 @@ exports.addMovie = (req, res) => {
 
         if(fileSize > 5000000) return res.status(422).json({message:'Image must be less than 5 MB'})
 
-        const filePath = path.join(__dirname, 'public', 'images', fileName);
-
-        MovieImage.mv(filePath, async(err) => {
+        MovieImage.mv(`./public/images/${fileName}`, async(err) => {
             if(err) return res.status(500).json({message: err.message})
 
             try{
